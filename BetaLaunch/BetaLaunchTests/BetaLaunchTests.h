5a0da305bcbac213915c7ce7f76a04c2a1984400
Robert DeNicola <thedizzle23@gmail.com>
2012-06-10 07:47:04 -0400

    yoooo there

---
+++ BetaLaunch/BetaLaunchTests/BetaLaunchTests.h
@@ -0,0 +1,13 @@
+//
+//  BetaLaunchTests.h
+//  BetaLaunchTests
+//
+//  Created by Robert DeNicola on 6/10/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BetaLaunchTests : SenTestCase
+
+@end
---
+++ BetaLaunch/BetaLaunchTests/BetaLaunchTests.m
@@ -0,0 +1,32 @@
+//
+//  BetaLaunchTests.m
+//  BetaLaunchTests
+//
+//  Created by Robert DeNicola on 6/10/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "BetaLaunchTests.h"
+
+@implementation BetaLaunchTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BetaLaunchTests");
+}
+
+@end
